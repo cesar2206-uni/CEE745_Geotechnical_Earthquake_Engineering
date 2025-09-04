259be9ef9ce1005211db38620aa85edc0edc8e59
César Manuel Sánchez Oré <81317084+cesar2206-uni@users.noreply.github.com>
2025-09-03 23:01:14 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title = "CEE 745 Geotechnical Earthquake Engineering")
 
 st.title("CEE 745 Geotechnical Earthquake Engineering")
 st.write("Notes and Homeworks from the couse CEE745 Fall 2025 UNR / Ramin Motamed, PhD PE (Proffesor)")
+
 st.write("By: Cesar Sanchez M.Sc. Student")
 
-st.sidebar.success("Pages")
 
